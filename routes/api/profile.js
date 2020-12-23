4043e3ce74b2f7b6bc059f77dfe63c5d16542b0c
Arppit Madankar <arppitmadankar@gmail.com>
2020-12-23 18:20:45 +0530

    Completing profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -35,7 +35,56 @@ async  (req , res ) => {
     if (!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        instagram,
+        linkedIn
+    } = req.body;
 
+    // build profile object
+    const profileFeilds = {}
+    profileFeilds.user = req.userid;
+    if (company) profileFeilds.company = company;
+    if (website) profileFeilds.website = website;
+    if (location) profileFeilds.location = location;
+    if (bio) profileFeilds.bio = bio;
+    if (status) profileFeilds.status = status;
+    if (githubusername) profileFeilds.githubusername = githubusername;
+    if (skills) {
+        profileFeilds.skills = skills.split(',').map(skill => skill.trim());
+    profileFeilds.social ={}
+    if (youtube) profileFeilds.social.youtube = youtube;
+    if (facebook) profileFeilds.social.twitter = facebook;
+    if (instagram) profileFeilds.social.instagram = instagram;
+    if (linkedIn) profileFeilds.social.linkedIn= linkedIn;
+   
+    try {
+        let profile = await Profile.findOne({user: req.user.id});
+        if(profile){
+            profile = await Profile.findOneAndUpdate({user : req.user.id}, {$set : profileFeilds }, {new :true});
+            return res.json(profile);
+        }
+        profile = new Profile(profileFeilds);
+
+        await profile.save()
+        res.json(profile);
+    }
+
+   
+    catch(err){
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
+
+}
 });
 
 
